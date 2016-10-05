9b6714457d56694072c57eb31c6a99f1598b43da
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2016-10-05 15:15:48 +0900

    minimum settings

--- sample-oauth-server-env/src/main/resources/database/H2-dataload.sql
+++ sample-oauth-server-env/src/main/resources/database/H2-dataload.sql
@@ -1,2 +1,6 @@
-/* load the records. */
+INSERT INTO users(username, password, enabled) VALUES('demo', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', true);
+INSERT INTO authorities(username, authority) VALUES('demo', 'read');
+INSERT INTO authorities(username, authority) VALUES('demo', 'create');
+INSERT INTO authorities(username, authority) VALUES('demo', 'update');
+INSERT INTO authorities(username, authority) VALUES('demo', 'delete');
 commit;
--- sample-oauth-server-env/src/main/resources/database/H2-schema.sql
+++ sample-oauth-server-env/src/main/resources/database/H2-schema.sql
@@ -1,1 +1,13 @@
-/* define the schemas. */
+create table if not exists users (
+	username varchar_ignorecase(50) not null primary key,
+	password varchar_ignorecase(60) not null,
+	enabled boolean not null
+);
+
+create table if not exists authorities (
+	username varchar_ignorecase(50) not null,
+	authority varchar_ignorecase(50) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
+
+create unique index ix_auth_username on authorities (username,authority);
