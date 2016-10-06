c72bcf53544dfc58da66621978682b76b3a56797
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2016-10-06 13:14:24 +0900

    set password encoder to clientDetailsUserService

--- sample-oauth-server-env/src/main/resources/database/H2-dataload.sql
+++ sample-oauth-server-env/src/main/resources/database/H2-dataload.sql
@@ -4,7 +4,7 @@ INSERT INTO authorities(username, authority) VALUES('demo', 'CREATE');
 INSERT INTO authorities(username, authority) VALUES('demo', 'UPDATE');
 INSERT INTO authorities(username, authority) VALUES('demo', 'DELETE');
 
-INSERT INTO clients(client_id, client_secret, client_name) VALUES('testClient', 'testClientSecret', 'hogehoge');
+INSERT INTO clients(client_id, client_secret, client_name) VALUES('testClient', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', 'hogehoge');
 INSERT INTO scopes(client_id, scope) VALUES('testClient', 'READ');
 INSERT INTO resources(client_id, resource_id) VALUES('testClient', 'testResource');
 INSERT INTO grants(client_id, grant) VALUES('testClient', 'PASSWORD');
