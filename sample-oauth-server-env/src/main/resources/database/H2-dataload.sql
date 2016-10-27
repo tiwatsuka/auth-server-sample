852b3c17ee6d35002444460aec9da3dea076066f
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2016-10-27 16:56:18 +0900

    change user's authority

--- sample-oauth-server-env/src/main/resources/database/H2-dataload.sql
+++ sample-oauth-server-env/src/main/resources/database/H2-dataload.sql
@@ -1,8 +1,8 @@
 INSERT INTO users(username, password, enabled) VALUES('demo', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', true);
-INSERT INTO authorities(username, authority) VALUES('demo', 'READ');
-INSERT INTO authorities(username, authority) VALUES('demo', 'CREATE');
-INSERT INTO authorities(username, authority) VALUES('demo', 'UPDATE');
-INSERT INTO authorities(username, authority) VALUES('demo', 'DELETE');
+INSERT INTO users(username, password, enabled) VALUES('demo2', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', true);
+INSERT INTO authorities(username, authority) VALUES('demo', 'ROLE_USER');
+INSERT INTO authorities(username, authority) VALUES('demo', 'ROLE_ADMIN');
+INSERT INTO authorities(username, authority) VALUES('demo2', 'ROLE_USER');
 
 INSERT INTO clients(client_id, client_secret, client_name) VALUES('testClient', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK', 'hogehoge');
 INSERT INTO scopes(client_id, scope) VALUES('testClient', 'READ');
